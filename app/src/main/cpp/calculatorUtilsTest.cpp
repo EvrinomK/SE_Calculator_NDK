90f1d4a8fa4ef1456511ab1c8e1f46b6e49c3297
Kirill Karetnyk <kirill.karetnyk@apriorit.com>
2021-03-10 14:18:52 +0200

    create green test calculateImpl_returns_ambigous_number

--- app/src/main/cpp/calculatorUtilsTest.cpp
+++ app/src/main/cpp/calculatorUtilsTest.cpp
@@ -12,3 +12,11 @@ TEST(calculatorUtils, calculateImpl_returns_double_for_str_only_with_double) {
 TEST(calculatorUtils, calculateImpl_returns_negative_number_for_str_only_with_negative_number) {
     ASSERT_EQ(-4, calculateImpl("-4"));
 }
+
+TEST(calculatorUtils, calculateImpl_returns_ambigous_number) {
+    ASSERT_EQ(4827, calculateImpl("4827"));
+}
+
+//TEST(calculatorUtils, calculateImpl_returns_sum_natural_numbers) {
+//    ASSERT_EQ(5, calculateImpl("2+3"));
+//}
