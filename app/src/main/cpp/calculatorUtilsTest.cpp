75965b1228a7918271aac313bbb13205f91592d3
Kirill Karetnyk <kirill.karetnyk@apriorit.com>
2021-03-10 14:14:37 +0200

    create green test calculateImpl_returns_negative_number_for_str_only_with_negative_number

--- app/src/main/cpp/calculatorUtilsTest.cpp
+++ app/src/main/cpp/calculatorUtilsTest.cpp
@@ -8,3 +8,7 @@ TEST(calculatorUtils, calculateImpl_returns_int_for_str_only_with_int) {
 TEST(calculatorUtils, calculateImpl_returns_double_for_str_only_with_double) {
     ASSERT_EQ(4.02, calculateImpl("4.02"));
 }
+
+TEST(calculatorUtils, calculateImpl_returns_negative_number_for_str_only_with_negative_number) {
+    ASSERT_EQ(-4, calculateImpl("-4"));
+}
