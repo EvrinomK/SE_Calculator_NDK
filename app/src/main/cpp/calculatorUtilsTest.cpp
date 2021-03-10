48a1be2b0b618ba3bfe1bcf39e3b70011d373cd5
Kirill Karetnyk <kirill.karetnyk@apriorit.com>
2021-03-10 15:16:48 +0200

    refactoring

--- app/src/main/cpp/calculatorUtilsTest.cpp
+++ app/src/main/cpp/calculatorUtilsTest.cpp
@@ -6,7 +6,7 @@ TEST(calculatorUtils, calculateImpl_returns_int_for_str_only_with_int) {
 }
 
 TEST(calculatorUtils, calculateImpl_returns_double_for_str_only_with_double) {
-    ASSERT_EQ(4.02, calculateImpl("4.02"));
+    ASSERT_DOUBLE_EQ(4.02, calculateImpl("4.02"));
 }
 
 TEST(calculatorUtils, calculateImpl_returns_negative_number_for_str_only_with_negative_number) {
@@ -22,7 +22,7 @@ TEST(calculatorUtils, calculateImpl_returns_sum_natural_numbers) {
 }
 
 TEST(calculatorUtils, calculateImpl_returns_sum_double_numbers) {
-    ASSERT_EQ(5.5, calculateImpl("2.3+3.2"));
+    ASSERT_DOUBLE_EQ(5.5, calculateImpl("2.3+3.2"));
 }
 
 TEST(calculatorUtils, calculateImpl_returns_diff_numbers_when_left_value_more_then_right) {
