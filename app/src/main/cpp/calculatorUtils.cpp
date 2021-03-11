4d925543b3a9e2073f8e3e1fd8e571086deafa18
Kirill Karetnyk <kirill.karetnyk@apriorit.com>
2021-03-11 16:19:31 +0200

    make green

--- app/src/main/cpp/calculatorUtils.cpp
+++ app/src/main/cpp/calculatorUtils.cpp
@@ -3,16 +3,17 @@
 
 namespace {
     Operators convertString2Operator(char symbol) {
-        if (symbol == '+') {
-            return Operators::Sum;
-        } else if (symbol == '-') {
-            return Operators::Minus;
-        } else if (symbol == '*') {
-            return Operators::Multiplication;
-        } else if (symbol == '/') {
-            return Operators::Div;
-        } else {
-            throw std::runtime_error("Cannot parse operator");
+        switch (symbol) {
+            case '+':
+                return Operators::Sum;
+            case '-':
+                return Operators::Minus;
+            case '*':
+                return Operators::Multiplication;
+            case '/':
+                return Operators::Div;
+            default:
+                throw std::runtime_error("Cannot parse operator");
         }
     }
 
@@ -25,6 +26,27 @@ namespace {
         return currentOperIndex + 1 < operatorsIndexes.size() &&
                (operatorsIndexes[currentOperIndex + 1].second == Multiplication ||
                 operatorsIndexes[currentOperIndex + 1].second == Div);
+    }
+
+    bool isUnarMinus(const std::vector<std::pair<size_t, Operators>> &operatorsIndexes,
+                     int currentOperIndex) {
+        return operatorsIndexes[currentOperIndex].second == Minus &&
+               (operatorsIndexes[currentOperIndex].first == 0 ||
+                operatorsIndexes[currentOperIndex - 1].first ==
+                operatorsIndexes[currentOperIndex].first - 1);
+    }
+
+    int findIndexNextLessPriorityOperator(
+            const std::vector<std::pair<size_t, Operators>> &operatorsIndexes,
+            int currentOperIndex) {
+        for (int i = currentOperIndex + 1; i < operatorsIndexes.size(); ++i) {
+            if (operatorsIndexes[i].second == Sum ||
+                (operatorsIndexes[i].second == Minus && !isUnarMinus(operatorsIndexes, i))) {
+                return i;
+            }
+        }
+
+        return currentOperIndex;
     }
 }
 
@@ -51,7 +73,14 @@ double calculateImpl(const std::string &expr) {
         switch (operatorsIndexes[i].second) {
             case Operators::Sum:
                 if (rightValueIsComplexExpr(operatorsIndexes, i)) {
-                    return result + calculateImpl(expr.substr(operatorsIndexes[i].first + 1));
+                    int nextLessOpIndex = findIndexNextLessPriorityOperator(operatorsIndexes, i);
+                    if (nextLessOpIndex == i) {
+                        return result + calculateImpl(expr.substr(operatorsIndexes[i].first + 1));
+                    } else {
+                        result += calculateImpl(expr.substr(operatorsIndexes[i].first + 1,
+                                                            operatorsIndexes[nextLessOpIndex].first - operatorsIndexes[i].first - 1));
+                        i = nextLessOpIndex - 1;
+                    }
                 } else {
                     result += std::stod(expr.substr(operatorsIndexes[i].first));
                 }
@@ -59,11 +88,19 @@ double calculateImpl(const std::string &expr) {
                 break;
             case Operators::Minus:
                 if (operatorsIndexes[i].first == 0 ||
-                    isOperator(expr[operatorsIndexes[i].first - 1])) {
+                    operatorsIndexes[i - 1].first == operatorsIndexes[i].first - 1) {
                     //ignore first minus because stod already parsed it
                     continue;
                 } else if (rightValueIsComplexExpr(operatorsIndexes, i)) {
-                    return result - calculateImpl(expr.substr(operatorsIndexes[i].first + 1));
+                    int nextLessOpIndex = findIndexNextLessPriorityOperator(operatorsIndexes, i);
+                    if (nextLessOpIndex == i) {
+                        return result - calculateImpl(expr.substr(operatorsIndexes[i].first + 1));
+                    } else {
+                        std::string complexExpr = expr.substr(operatorsIndexes[i].first + 1,
+                                                              operatorsIndexes[nextLessOpIndex].first - operatorsIndexes[i].first - 1);
+                        result -= calculateImpl(complexExpr);
+                        i = nextLessOpIndex - 1;
+                    }
                 } else {
                     result -= std::stod(expr.substr(operatorsIndexes[i].first + 1));
                 }
